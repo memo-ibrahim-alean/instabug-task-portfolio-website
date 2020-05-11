200ca9a19be8289fdb2b30855e0036c1e8555c71
Mohamed Ibraim <mohamedibrahim16698@gmail.com>
2020-05-11 16:05:14 +0200

    install font awesome and add linkidin and github links

--- src/sections/contact/contact.js
+++ src/sections/contact/contact.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithubAlt,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
 import './contact.css';
 
 class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+  }
+
   render() {
     return (
       <div id='contactSection'>
@@ -16,20 +29,58 @@ class Contact extends Component {
                 <Form>
                   <Form.Group controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control size="lg" type="text" placeholder="Enter name" required />
+                    <Form.Control
+                      name='name'
+                      size="lg"
+                      type="text"
+                      placeholder="Enter name"
+                      required />
                   </Form.Group>
                   <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control size="lg" type="email" placeholder="Email" required />
+                    <Form.Control
+                      name='email'
+                      size="lg"
+                      type="email"
+                      placeholder="Email"
+                      required />
                   </Form.Group>
                   <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Message</Form.Label>
-                    <Form.Control size="lg" as="textarea" rows="3" />
+                    <Form.Control
+                      name='message'
+                      size="lg"
+                      as="textarea"
+                      rows="3" />
                   </Form.Group>
-                  <Button className='btn-lg btn-block' variant="primary" type="submit">
+                  <Button  className='btn-lg btn-block' variant="primary" type="submit">
                     Submit
                   </Button>
                 </Form>
+              </Col>
+            </Row>
+            <Row className='mt-5'>
+              <Col sm="12">
+                <div>
+                  <FontAwesomeIcon
+                    icon={faGithubAlt}
+                    style={{ color: "#333" }}
+                    size='3x'
+                    className='mr-4'
+                  />
+                  <a href='https://www.linkedin.com/in/mohamedibrahim1998/'
+                    target='_blank'
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      style={{ color: "#0e76a8" }}
+                      icon={faLinkedin}
+                      size='3x' />
+                  </a>
+                </div>
+                <p className='center mt-4 h5'>
+                  Built with <span role="img" aria-label="Heart">❤️</span> by Mohamed Ibrahim
+                </p>
               </Col>
             </Row>
           </Container>
