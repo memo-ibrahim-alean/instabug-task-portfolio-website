a191d2591bef57a0ed19a0b7d9fc03841b3f0e4d
Mohamed Ibraim <mohamedibrahim16698@gmail.com>
2020-05-11 04:27:46 +0200

    add shadow to nav and add mr-3 to it's links

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,15 +4,15 @@ import { HashLink as Link } from 'react-router-hash-link';
 import './navbar.css';
 
 const NavBar = () => (
-    <Navbar collapseOnSelect sticky="top" expand="lg" bg="light" variant="light">
+  <Navbar className='shadow bg-white rounded' collapseOnSelect sticky="top" expand="lg" bg="light" variant="light">
     <Link to='/#header' className='logo'>M.Ibrahim</Link>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-        <Link to='/#projectSection' className='nav-link'>Projects</Link>
-        <Link to='/#skillsSection' className='nav-link'>Skills</Link>
-        <Link to='/#aboutSection' className='nav-link'>About</Link>
-        <Link to='/#contactSection' className='nav-link'>Contact</Link>
+        <Link to='/#projectSection' className='nav-link mr-3`'>Projects</Link>
+        <Link to='/#skillsSection' className='nav-link mr-3'>Skills</Link>
+        <Link to='/#aboutSection' className='nav-link mr-3'>About</Link>
+        <Link to='/#contactSection' className='nav-link mr-3'>Contact</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
