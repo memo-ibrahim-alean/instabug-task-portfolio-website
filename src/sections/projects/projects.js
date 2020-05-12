52cfdd2ed0c304a632794b3ba485a4aa2228ba26
Mohamed Ibraim <mohamedibrahim16698@gmail.com>
2020-05-12 21:26:32 +0200

    update projects cards UI

--- src/sections/projects/projects.js
+++ src/sections/projects/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
 import { HashLink as Link } from 'react-router-hash-link';
 import getData from '../../data/getData';
 import './projects.css';
@@ -27,26 +27,22 @@ class Projects extends Component {
           <Row>
             {
               projects.map(project => (
-                <div key={project.id} className="card mb-3 text-capitalize shadow" style={{ maxWidth: '100%' }}>
-                  <div className="row no-gutters">
-                    <div className="col-md-2 text-center">
-                      <img src={project.Photo} className="card-img" alt="..." />
-                    </div>
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="card-title"> {project.title} </h5>
-                        <p className="card-text"> {project.overView} </p>
-                        <Link
-                          to={"/projects-details/" + project.id}
-                          className="btn btn-link"
-                        >
-                          More Details 
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
+                <Card key={project.id} className='mb-4 col-sm-4'>
+                  <Card.Body className='text-center'>
+                    <Card.Title >
+                      {project.title}
+                    </Card.Title>
+                    <Card.Text>
+                      {project.overView}
+                    </Card.Text>
+                    <Link
+                      to={"/projects-details/" + project.id}
+                      className="btn btn-outline-primary"
+                    >
+                      More Details
+                    </Link>
+                  </Card.Body>
+                </Card>
               ))
             }
           </Row>
